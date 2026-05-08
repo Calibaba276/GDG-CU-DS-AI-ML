7938569d267b89873bde79349859655e52750fca
Munaaaa95 <nwosumuna95@gmail.com>
2026-05-08 09:34:22 +0100

    Complete Mission 01 submission

--- mission_01/main.py
+++ mission_01/main.py
@@ -23,10 +23,8 @@ while True:
     if total_spent > budget:
         print(" Warning: You are over budget")
 
-# Calculate remaining budget
 remaining_budget = budget - total_spent
 
-# Final summary
 
 print(f"\nTotal spent: ${total_spent:.2f}")
 print(f"Budget remaining: ${remaining_budget:.2f}")
